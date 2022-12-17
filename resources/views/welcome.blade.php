be4804b16e3bcf91fdc1a9b707c2d4cd8d65e47b
Alvin <alvinrw04@gmail.com>
2022-12-17 07:13:55 +0700

    TPM BE 1

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>HOME</h1>
+    <h2>Home</h2>
+    <h3>HomE</h3>
+    <h4>hOMe</h4>
+    <h5>hoMe</h5>
+    <h6>homE</h6>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, quasi pariatur, minima commodi ipsum consectetur adipisci quod est consequatur tempora sunt molestiae ducimus laboriosam quia dignissimos saepe qui vitae? Voluptates.</p>
+    <a href="https://www.google.com/" target="blank">Google</a>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores in cumque ut obcaecati assumenda rerum suscipit est deleniti tenetur atque <br>consequuntur quod, ipsum autem omnis! Dignissimos aut harum ipsa assumenda! <hr></p>
+    <table border="1">
+        <tr>
+            <th>Nama</th>
+            <th>Umur</th>
+        </tr>
+        <tr>
+            <td>Yeremia</td>
+            <td>20</td>
+        </tr>
+    </table>
+    <div style="background-color:blue">
+        Halo World
+    </div>
+    <ul>
+        <li>hola</li>
+        <li>halo</li>
+    </ul>
+    <ol>
+        <li>hola</li>
+        <li>halo</li>
+    </ol>
+
+    <form action="">
+        <input type="text">
+        <input type="password">
+        <input type="number">
+        <button>submit</button>
+    </form>
+</body>
+</html>
